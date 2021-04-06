6da108775614717bc7ef3db5c5dee59cf48e3677
Aron Hemmes <37915022+Alpacron@users.noreply.github.com>
2021-04-06 18:20:36 +0200

    Fix covered lines not properly generating

--- src/Util/convertToD3Graph.tsx
+++ src/Util/convertToD3Graph.tsx
@@ -3,12 +3,9 @@ import {GraphData} from "react-d3-graph";
 function getCover(graph: any, vertex: number, depth: number, currentDepth: number, covered: { source: number, target: number }[]) {
     if (currentDepth < depth) {
         graph[vertex.toString()].forEach((v: number) => {
-            if (covered.filter(c => (c.source === v && c.target === vertex) || (c.source === vertex && c.target === v)).length === 0) {
-                let c = [];
-                covered.forEach(val => c.push(Object.assign({}, val)));
-                c.push({source: vertex, target: v});
-                covered = getCover(graph, v, depth, currentDepth + 1, c)
-            }
+            if (covered.filter(c => (c.source === v && c.target === vertex) || (c.source === vertex && c.target === v)).length === 0)
+                covered.push({source: vertex, target: v});
+            covered = getCover(graph, v, depth, currentDepth + 1, covered);
         })
     }
 
@@ -19,8 +16,9 @@ export default function convertToD3Graph(graph: any, depth: number, cover: numbe
     let nodes: { id: number, color: string }[] = [];
     let links: { source: number, target: number, color: string }[] = [];
     let covered: { source: number, target: number }[] = [];
-    if (cover.length > 0)
+    if (cover.length > 0) {
         cover.forEach(c => covered = getCover(graph, c, depth, 0, covered));
+    }
 
     Object.keys(graph).forEach(v => {
         nodes.push({id: +v, color: cover.includes(+v) ? "#3f51b5" : "#d3d3d3"});
