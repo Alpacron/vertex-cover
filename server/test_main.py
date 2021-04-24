bb83df5795a03a736a2dbced52aa83771195e3b6
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2021-04-24 13:07:19 +0200

    Update generate graph test

--- server/test_main.py
+++ server/test_main.py
@@ -9,16 +9,14 @@ def test_read_generate():
     response = client.post("/generate", json={"vertices": 10, "probability": 0})
     assert response.status_code == 200
     assert response.json() == {
-        "graph": {
-            "0": [],
-            "1": [],
-            "2": [],
-            "3": [],
-            "4": [],
-            "5": [],
-            "6": [],
-            "7": [],
-            "8": [],
-            "9": []
-        }
+        "0": [],
+        "1": [],
+        "2": [],
+        "3": [],
+        "4": [],
+        "5": [],
+        "6": [],
+        "7": [],
+        "8": [],
+        "9": []
     }
